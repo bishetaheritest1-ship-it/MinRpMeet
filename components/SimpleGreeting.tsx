ebf9b1d0e033653b6fb050df3fad375b411db996
bishetaheritest1-ship-it <bishe.taheri.test1@gmail.com>
2025-12-07 16:38:18 +0330

    Delete components directory

--- components/SimpleGreeting.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const SimpleGreeting: React.FC = () => {
-  return (
-    <h1 className="text-5xl font-extrabold text-indigo-600 drop-shadow-sm">
-      سلام!
-    </h1>
-  );
-};
